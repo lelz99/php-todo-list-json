e07074e4b9fb30f6c1680b21f672aeb052516251
lelz99 <emanuelesimonetti99@gmail.com>
2024-02-22 18:14:05 +0100

    generated dynamically by api

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php 
 
-?>
+$source_path = __DIR__ . '../../database/tasks.json';
+
+$tasks_json = file_get_contents($source_path);
+
+$tasks = json_decode($tasks_json, true);
+
+header('Content-Type: application/json');
+
+echo json_encode($tasks); 
--- my_app/js/script.js
+++ my_app/js/script.js
@@ -1,7 +1,16 @@
 const { createApp } = Vue;
 
-const app = createApp({
+const endpoint = 'http://localhost/boolean/php-todo-list-json/api';
 
+const app = createApp({
+    data: () => ({
+        tasks: []
+    }),
+    created() {
+        axios.get(endpoint).then(res => {
+            this.tasks = res.data
+        })
+    }
 })
 
 app.mount('#root')
